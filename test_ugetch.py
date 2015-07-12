781f808e47447b05f765a5ab17a07baa27165e59
toejough <toejough@gmail.com>
2015-07-12 15:27:34 -0400

    Summary:  Fixes #6 - adding unicode support
    **Problem:**
    No unicode support
    
    **Analysis:**
    UTF-8 is the most widespread and sensible unicode encoding.  Add support
    for it.
    
    **Testing:**
    Added tests which check a symbol, greek, and tibetan glyphs
    
    **Documentation:**
    Updated the readme and inline docs

--- test_ugetch.py
+++ test_ugetch.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 '''
 Test suite for ugetch
 '''
@@ -7,6 +10,7 @@ Test suite for ugetch
 # [ - Python ]
 import tempfile
 import os
+import sys
 # [ - Third Party ]
 import pytest
 import pexpect
@@ -17,11 +21,17 @@ import ugetch
 # [ Helpers ]
 def in_vs_out(input_values):
     '''supply ascii chars, use getch, and validate ascii returned'''
-    p = pexpect.spawn("python ugetch-cli.py", timeout=0.5)
+    # choose the correct spawn
+    spawn = pexpect.spawn
+    if sys.version_info.major == 3:
+        spawn = pexpect.spawnu
+    # spawn the subproc
+    p = spawn("python ugetch-cli.py", timeout=0.5)
     p.expect_exact("hit ctrl-c to exit.")
+    # test each char
     for value in input_values:
         p.expect_exact("hit a key to print its representation: ")
-        p.sendline(value)
+        p.send(value)
         index = p.expect_exact([value, pexpect.EOF, pexpect.TIMEOUT])
         assert index == 0, "did not find expected ({}) in output ({})".format(
             value, p.before
@@ -34,3 +44,14 @@ def test_getch_ascii():
     in_vs_out(" \n\t")
     all_printable_acii = [chr(n) for n in range(32, 127)]  # not including 127
     in_vs_out(all_printable_acii)
+
+def test_getch_utf8():
+    '''supply utf-8 glyphs, use getch, and validate the keys returned'''
+    in_vs_out(['☢'])
+    in_vs_out(['ά','έ','ή','ί','ΰ','α','β','γ','δ','ε','ζ','η','θ','ι','κ','λ','μ','ν','ξ','ο','π'])  # greek
+    in_vs_out(['༰','༱','༲','༳','༴',' ༵','༶','༸',' ༹','༺',' ','༻',' ','༼','༽',' ༾',' ༿',' ','ཀ','ཁ','ག','གྷ','ང','ཅ','ཆ'])  # tibetan
+
+
+if __name__ == '__main__':
+    import cProfile
+    cProfile.run("test_getch_ascii()")
--- ugetch/__init__.py
+++ ugetch/__init__.py
@@ -10,25 +10,74 @@ import sys
 import termios
 
 
+# [ Parsers ]
+def ascii_parser(infile):
+    '''Parse ascii bytes to ascii keys'''
+    byte = _get_byte(infile)
+    if byte < 128:
+        return chr(byte)
+    else:
+        _put_byte(byte)
+
+
+def utf8_parser(infile):
+    '''Parse utf-8 bytes to a string'''
+    first_byte = _get_byte(infile)
+    # get byte string in py 2 and 3
+    if sys.version_info.major == 3:
+        string = chr(first_byte)
+    else:
+        utf8_bytes = [first_byte]
+        if 194 <= first_byte <= 223:
+            # 2 bytes
+            utf8_bytes.append(_get_byte(infile))
+        elif 224 <= first_byte <= 239:
+            # 3 bytes
+            utf8_bytes.append(_get_byte(infile))
+            utf8_bytes.append(_get_byte(infile))
+        elif 240 <= first_byte <= 244:
+            # 4 bytes
+            utf8_bytes.append(_get_byte(infile))
+            utf8_bytes.append(_get_byte(infile))
+            utf8_bytes.append(_get_byte(infile))
+        byte_string = ''.join([chr(b) for b in utf8_bytes])
+        string = byte_string.decode('utf-8')
+    return string
+
+
 # [ Global ]
 _DEFAULT=object()  # enable dynamic defaults
+_KEY_PARSERS=[ascii_parser, utf8_parser]  # ways to parse keys from byte lists
+_BYTES=[]  # byte buffer from the input file
 
 
 # [ Helper Functions ]
+def _put_byte(byte):
+    '''Put a single byte onto the buffer'''
+    global _BYTES
+    _BYTES.append(byte)
+
+
 def _get_byte(infile):
-    fd = infile.fileno()
-    old_settings = termios.tcgetattr(fd)
-    byte = None
-    try:
-        new = termios.tcgetattr(fd)
-        new[3] = new[3] & ~termios.ECHO
-        new[3] = new[3] & ~termios.ICANON
-        termios.tcsetattr(fd, termios.TCSADRAIN, new)
-        byte = infile.read(1)
-    # no catching - don't know what could go wrong, so no good way to handle it right now.
-    # let it propagate up and generate a bug report.
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    '''Get a single byte from either the buffer (if non-empty), or the input stream'''
+    if _BYTES:
+        byte = _BYTES[0]
+        del _BYTES[0]
+    else:
+        fd = infile.fileno()
+        old_settings = termios.tcgetattr(fd)
+        char = None
+        try:
+            new = termios.tcgetattr(fd)
+            new[3] = new[3] & ~termios.ECHO
+            new[3] = new[3] & ~termios.ICANON
+            termios.tcsetattr(fd, termios.TCSADRAIN, new)
+            char = infile.read(1)
+        # no catching - don't know what could go wrong, so no good way to handle it right now.
+        # let it propagate up and generate a bug report.
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        byte = ord(char)
     return byte
 
 
@@ -39,5 +88,14 @@ def getkey(infile=_DEFAULT):
     if infile == _DEFAULT:
         # if not overridden, use the current sys.stdin
         infile = sys.stdin
-    # Do the standard getch
-    return _get_byte(infile)
+    # Set some defaults
+    key = None
+    byte_list = []
+    key_type = None
+    # Loop through parsers and return the first that matches
+    for parser in _KEY_PARSERS:
+        key = parser(infile)
+        if key is not None:
+            break
+    # Return the key to the user
+    return key
